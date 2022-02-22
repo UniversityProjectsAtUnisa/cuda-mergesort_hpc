f1391fd730ba9dd71704bdc2bdb843ce65975eba
Earendil Tiwele <73339338+EarendilTiwele@users.noreply.github.com>
2022-02-22 16:06:45 +0100

    test with host sorted array

--- mergesort.cu
+++ mergesort.cu
@@ -21,11 +21,13 @@
 
 int _is_sorted(DATA *arr, size_t size);
 void MergeSortOnDevice(DATA *arr, size_t size);
+void MergeSortOnHost(DATA *arr, size_t size);
+void _merge(DATA *arr1, size_t size1, DATA *arr2, size_t size2, DATA *tmp);
 __global__ void gpu_mergesort(DATA *A, DATA *B, size_t size, size_t width);
 __device__ void gpu_bottomUpMerge(DATA *arr1, size_t size1, DATA *arr2,
                                   size_t size2, DATA *tmp);
 __global__ void gpu_mergesort_tasksize(DATA *arr, DATA *tmp, size_t size,
-                                      size_t tasksize);
+                                       size_t tasksize);
 __device__ int gpu_serial_merge_sort(DATA *arr, DATA *tmp, size_t n);
 
 int main(int argc, char **argv) {
@@ -44,15 +46,55 @@ int main(int argc, char **argv) {
   for (size_t i = 0; i < size; i++) {
     arr[i] = rand();  // TODO: generate with sign and maybe in a range
   }
-
+  DATA *hostArr;
+  hostArr = (DATA *)malloc(size * sizeof(DATA));
+  if (hostArr == NULL) {
+    fprintf(stderr, "Memory could not be allocated");
+    exit(EXIT_FAILURE);
+  }
+  memcpy(hostArr, arr, size * sizeof(DATA));
+  MergeSortOnHost(hostArr, size);
   MergeSortOnDevice(arr, size);
-  assert(_is_sorted(arr, size) == 1);
+  assert(memcmp(hostArr, arr, size * sizeof(DATA)));
 }
 
-int _is_sorted(DATA *arr, size_t size) {
-  for (size_t i = 0; i < size - 1; i++)
-    if (arr[i] > arr[i + 1]) return 0;
-  return 1;
+void MergeSortOnHost(DATA *arr, size_t n) {
+  if (n == 0) return;
+  DATA *tmp;
+
+  tmp = (DATA *)malloc(n * sizeof(DATA));
+  if (tmp == NULL) {
+    fprintf(stderr, "Memory could not be allocated");
+    exit(EXIT_FAILURE);
+  }
+
+  for (size_t curr_size = 1; curr_size <= n - 1; curr_size *= 2) {
+    for (size_t left_start = 0; left_start <= n - curr_size - 1;
+         left_start += 2 * curr_size) {
+      size_t right_size = MIN(curr_size, n - left_start - curr_size);
+
+      _merge(arr + left_start, curr_size, arr + left_start + curr_size,
+             right_size, tmp);
+    }
+  }
+}
+
+void _merge(DATA *arr1, size_t size1, DATA *arr2, size_t size2, DATA *tmp) {
+  size_t i = 0, j = 0;
+
+  while (i < size1 && j < size2) {
+    if (arr1[i] < arr2[j]) {
+      tmp[i + j] = arr1[i];
+      i++;
+    } else {
+      tmp[i + j] = arr2[j];
+      j++;
+    }
+  }
+  if (i < size1) memcpy(tmp + i + j, arr1 + i, (size1 - i) * sizeof(DATA));
+  if (j < size2) memcpy(tmp + size1 + j, arr2 + j, (size2 - j) * sizeof(DATA));
+
+  memcpy(arr1, tmp, (size1 + size2) * sizeof(DATA));
 }
 
 void MergeSortOnDevice(DATA *arr, size_t size) {
